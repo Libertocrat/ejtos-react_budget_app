048fd64ee7b39dc8e146c113a2069a44de70f180
Libertocrat <libertocrat@proton.me>
2024-06-05 18:42:45 -0400

    Update: Complete Task 7 - Decrease btn & btn styles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -25,12 +25,66 @@ const ExpenseItem = (props) => {
 
     }
 
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    /*** INTERNAL CSS ***/
+    const plusButtonStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius : '50%',
+        background : '#65A30D', //Lime 600
+        cursor: 'pointer',
+
+        width : '40px',
+        height: '40px'
+    };
+
+    const minusButtonStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius : '50%',
+        background : '#B91C1C', //Red 700
+        cursor: 'pointer',
+
+        width : '40px',
+        height: '40px'
+    };
+
+    const buttonTextStyle = {
+        color: 'white',
+        fontSize: '36px',
+        fontWeight: '900',
+    };
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        <td>
+            <div style={plusButtonStyle} onClick={event=> increaseAllocation(props.name)}>
+                <div style={buttonTextStyle} >+</div>
+            </div>
+        </td>
+        <td>
+            <div style={minusButtonStyle} onClick={event=> decreaseAllocation(props.name)}>
+                <div style={{background:'white',height:'6px', width:'20px'}} ></div>
+            </div>
+        </td>
+        <td><TiDelete size='40px' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
